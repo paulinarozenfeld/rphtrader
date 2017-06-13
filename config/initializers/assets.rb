1f65890518b89b61d6a5bd4a5af52c682a30ab91
[paulinaroznfeld] <[paulinarozenfeld@gmail.com]>
2017-06-12 23:30:39 -0400

    set up config vars

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -12,3 +12,22 @@ Rails.application.config.assets.paths << Rails.root.join('node_modules')
 # application.js, application.css, and all non-JS/CSS in the app/assets
 # folder are already added.
 # Rails.application.config.assets.precompile += %w( admin.js admin.css )
+
+Rails.application.config.assets.precompile += %w( bootstrap.min.css )
+Rails.application.config.assets.precompile += %w( animate.min.css )
+Rails.application.config.assets.precompile += %w( font-awesome.min.css )
+Rails.application.config.assets.precompile += %w( owl.carousel.min.css )
+Rails.application.config.assets.precompile += %w( owl.theme.default.min.css )
+Rails.application.config.assets.precompile += %w( settings.css )
+Rails.application.config.assets.precompile += %w( offcanvas-menu.css )
+Rails.application.config.assets.precompile += %w( style.css )
+
+
+Rails.application.config.assets.precompile += %w( bootstrap.min.js )
+Rails.application.config.assets.precompile += %w( jquery.themepunch.tools.min.js )
+Rails.application.config.assets.precompile += %w( jquery.themepunch.revolution.min.js )
+Rails.application.config.assets.precompile += %w( jquery.inview.min.js )
+Rails.application.config.assets.precompile += %w( wow.js )
+Rails.application.config.assets.precompile += %w( lightbox.js )
+Rails.application.config.assets.precompile += %w( owl.carousel.min.js )
+Rails.application.config.assets.precompile += %w( script.js )
