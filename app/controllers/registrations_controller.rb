e1cac4d76511843a53a7176f36d3450c94b2cb60
Elan Kvitko <elan@idknyc.com>
2017-06-14 12:42:45 -0400

    custom devise controllers

---
+++ app/controllers/registrations_controller.rb
@@ -0,0 +1,32 @@
+class RegistrationsController < Devise::RegistrationsController
+  def new
+    super
+  end
+
+  def create
+    user = User.new( create_params )
+
+    if user.save
+      sign_in( :user, user )
+      redirect_to root_path
+    end
+  end
+
+  def update
+    super
+  end
+
+  def edit
+    super
+  end
+
+  def destroy
+    super
+  end
+
+    private
+
+      def create_params
+        params.require( :user ).permit( :first_name, :last_name, :pharmacy_name, :role, :password, :password_confirmation, :email, :alias )
+      end
+end
