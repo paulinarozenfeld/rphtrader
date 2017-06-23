6eb8b9683d1183981eefce369349e02e79fd93d1
Elan Kvitko <elan@idknyc.com>
2017-06-23 13:58:52 -0400

    fix message send bug

--- app/controllers/conversations_controller.rb
+++ app/controllers/conversations_controller.rb
@@ -44,6 +44,6 @@ class ConversationsController < ApplicationController
   private
 
   def conversation_params
-    params.require(:conversation).permit(:subject, :body,recipients:[])
+    params.require(:conversation).permit(:subject, :body, :recipients)
   end
 end
